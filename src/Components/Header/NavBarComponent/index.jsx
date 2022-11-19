e469f54b5ebfe2f4816d0aeb81a732ed823886b5
SammuDev <samucas878@gmail.com>
2022-11-19 19:01:19 -0300

    .

--- src/Components/Header/NavBarComponent/index.jsx
+++ src/Components/Header/NavBarComponent/index.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import logo from './../images/logoWD.jpg';
+import logo from './../images/logoWD.png';
 
 export const NavBar = () => (
     <div>
