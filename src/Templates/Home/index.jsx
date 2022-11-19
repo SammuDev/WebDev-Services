6815083cc1e106b50fcdaae10143516b3c7ea430
SammuDev <samucas878@gmail.com>
2022-11-19 01:50:29 -0300

    Criadno components e template

---
+++ src/Templates/Home/index.jsx
@@ -0,0 +1,9 @@
+// import logo from './logo.svg';
+import './styleHome.css';
+import { Header } from './../../Components/Header';
+
+export const Home = () => (
+    <div>
+        <Header/>
+    </div>
+)
