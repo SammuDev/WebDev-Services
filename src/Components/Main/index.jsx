5b051275416ea19c7c0458f9450af3c844c16e03
SammuDev <samucas878@gmail.com>
2022-11-20 02:34:25 -0300

    Adicionando o React-Icons

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -1,8 +1,24 @@
 import { Carr } from './Carr';
+
 import './style.css';
 
-export const Main = () => (
-    <div className="box">
-        <Carr/>
-    </div>
-)
+export const Main = () => {
+
+    return (
+        <div className="main">
+            <section>
+                <Carr/>
+            </section>
+
+            <section className='sectionMain'>
+                <h1>Title</h1>
+
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </section>
+        </div>
+    )
+}
