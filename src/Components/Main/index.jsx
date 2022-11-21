5a0ed520fd6c9d0d8cc74258d94a6fbb9dec67d8
SammuDev <samucas878@gmail.com>
2022-11-21 14:36:24 -0300

    .

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -12,7 +12,7 @@ export const Main = () => {
             </section>
 
             <section className='sectionMain'>
-                <h1>Empreendedorismo: o que é, tipos, cenário do Brasil, vantagens e ideias</h1>
+                <h1>Empreendedorismo: o que é, tipos, cenário do Brasil, vantagens e ideias:</h1>
 
                 <div className='publicity'>
                     <figure>
@@ -20,12 +20,14 @@ export const Main = () => {
                     </figure>
                 </div>
 
-                <p>Resolveu se aventurar no empreendedorismo e quer saber os segredos do sucesso?</p>
-                <p>A verdade é que não existem fórmulas prontas, mas alguns atalhos que encurtam sua caminhada rumo ao sonho de se tornar bem-sucedido nos negócios.</p>
-                <p>Como principiante, você deve se focar no planejamento e se preparar para os desafios, além de assumir uma postura confiante e determinada.</p>
-                <p>Aqui, você vai encontrar orientações para facilitar os seus primeiros passos e guiar sua trajetória no empreendedorismo.</p>
-                <p>Lembre-se: o começo pode parecer difícil, mas ter a própria empresa vale todo o esforço!</p>
-                <p>Não é à toa que os jovens estão encarando o empreendedorismo cada vez mais cedo, como você vai descobrir ao longo do texto.</p>
+                <div className='text'>
+                    <p>Resolveu se aventurar no empreendedorismo e quer saber os segredos do sucesso?</p>
+                    <p>A verdade é que não existem fórmulas prontas, mas alguns atalhos que encurtam sua caminhada rumo ao sonho de se tornar bem-sucedido nos negócios.</p>
+                    <p>Como principiante, você deve se focar no planejamento e se preparar para os desafios, além de assumir uma postura confiante e determinada.</p>
+                    <p>Aqui, você vai encontrar orientações para facilitar os seus primeiros passos e guiar sua trajetória no empreendedorismo.</p>
+                    <p>Lembre-se: o começo pode parecer difícil, mas ter a própria empresa vale todo o esforço!</p>
+                    <p>Não é à toa que os jovens estão encarando o empreendedorismo cada vez mais cedo, como você vai descobrir ao longo do texto.</p>
+                </div>
             
             </section>
         </div>
